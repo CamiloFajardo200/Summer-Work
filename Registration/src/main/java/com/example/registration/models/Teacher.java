ff2060444a8d919fb3d95a0e036706b92684e6de
Zamokuhle23 <65773452+Zamokuhle23@users.noreply.github.com>
2021-07-07 15:44:34 +0300

    Add files via upload

---
+++ Registration/src/main/java/com/example/registration/models/Teacher.java
@@ -0,0 +1,31 @@
+package com.example.registration.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "teacher")
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
+
+    @ManyToMany(mappedBy = "teachers")
+    private List<Student> students;
+}
